c42144b03d60ca08844dd780773dd993169daadd
Heitor Rodrigues <heitormorak@gmail.com>
2021-08-21 22:39:16 -0300

    tarefa009

---
+++ tarefa009/Tarefa009_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tarefa009_5
+{
+    class Tarefa009_5
+    {
+        static void Main(string[] args)
+        {
+            double sm, su, qtd;
+
+            Console.WriteLine("Informe o salario minimo:");
+            sm = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe o salario do usuario:");
+            su = Convert.ToDouble(Console.ReadLine());
+
+            qtd = su / sm;
+            Console.WriteLine("O usuario ganha " + Math.Round(qtd, 2) + " salarios minimos");
+        }
+    }
+}
